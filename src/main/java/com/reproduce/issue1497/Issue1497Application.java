c31c13d9b725dfd5539ff364356c55a8aa383996
Valentin <bossi.valentin@gmail.com>
2023-04-30 14:26:27 +0000

    add basic deps for project

---
+++ src/main/java/com/reproduce/issue1497/Issue1497Application.java
@@ -0,0 +1,13 @@
+package com.reproduce.issue1497;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Issue1497Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Issue1497Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/reproduce/issue1497/Issue1497ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reproduce.issue1497;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Issue1497ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
